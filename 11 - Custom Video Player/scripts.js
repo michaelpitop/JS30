09f3e7964ca3325d1f3fd70955d7df1f0b2f7e25
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-10 15:02:48 +0200

    added icons for play and pause

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -13,7 +13,15 @@ function togglePlay() {
   video[method]();
 }
 
+function  updateButton() {
+  const icon = this.paused ? '▶' : '⏸';
+  toggle.textContent = icon;
+}
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+
 toggle.addEventListener('click', togglePlay);
