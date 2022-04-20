5df8e81e33c2883dec199fd0d56440e8c714537c
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-20 14:24:02 +0200

    testing the function

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,7 +1,7 @@
 let countdown;
 
 function timer(seconds) {
-  setInterval(function() {
-    seconds--;
-  }, 1000);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  console.log({now, then});
 }
