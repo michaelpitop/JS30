117dcf7f48eff03e9ea525974e8800670a4211d0
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-20 14:45:01 +0200

    .

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -37,3 +37,10 @@ function displayEndTime(timestamp) {
   const minutes = end.getMinutes();
   endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
