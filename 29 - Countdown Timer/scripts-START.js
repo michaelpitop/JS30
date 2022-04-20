c330a710bc8fad7bfd2cb0b869270b1def8e69aa
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-20 14:31:14 +0200

    .

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -3,5 +3,13 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({now, then});
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+  }, 1000);
 }
