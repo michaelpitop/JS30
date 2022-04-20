8b3c5c4800f616ae14a9305bb51309526a247bde
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-20 14:45:36 +0200

    end of challenge

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelector('[data-time]');
-
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -12,12 +14,13 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-
+    // check if we should stop it!
     if(secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    displayTimeLeft(seconds);
+    // display it
+    displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
@@ -25,10 +28,9 @@ function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-  document.title = display; // cool feature
+  document.title = display;
   timerDisplay.textContent = display;
 }
-
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
@@ -51,3 +53,4 @@ document.customForm.addEventListener('submit', function(e) {
   timer(mins * 60);
   this.reset();
 });
+
