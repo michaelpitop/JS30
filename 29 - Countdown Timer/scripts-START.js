38c435bf32c9cebec04d7acfbb5431fe145357c6
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-20 14:33:51 +0200

    added display time left function

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -3,6 +3,9 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
 
@@ -10,6 +13,19 @@ function timer(seconds) {
       clearInterval(countdown);
       return;
     }
-
+    displayTimeLeft(seconds);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }
